018f1f456af732204adf8b9e7e77f4379dbb5e07
Moriya Katan <moriyakatan@gmail.com>
2023-02-26 12:27:54 +0200

    new project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import TodoItem from './component/TodoItem';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Todo - List</h2>
+      <TodoItem />
+    </div>
+  );
+}
+
+export default App;
